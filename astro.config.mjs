f90e220cc342bb3f463bdda9f893fa0a06f313f0
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-01-05 21:44:17 -0800

    newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-frontend-masters-kcanamar.netlify.app',
+  output: "hybrid",
+  experimental: {
+    hybridOutput: true,
+  },
   integrations: [react(), solidJs()]
 });
