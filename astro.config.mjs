28bbd5c3f5e076b1f7069eb5526f200f2f25c474
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-01-05 21:54:38 -0800

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-frontend-masters-kcanamar.netlify.app',
   output: "hybrid",
-  experimental: {
-    hybridOutput: true
-  },
   integrations: [react(), solidJs()],
-  adapter: netlify()
+  adapter: vercel()
 });
