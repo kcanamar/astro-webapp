c70dfc1d42f56592822a6624a384505997750700
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-01-05 21:45:06 -0800

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-frontend-masters-kcanamar.netlify.app',
   output: "hybrid",
   experimental: {
-    hybridOutput: true,
+    hybridOutput: true
   },
-  integrations: [react(), solidJs()]
+  integrations: [react(), solidJs()],
+  adapter: netlify()
 });
